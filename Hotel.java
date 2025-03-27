4cdfc82354695332dfde955cb9022b3d7428b103
huligangatjf <tfh3293@students.uc-bcf.edu.ph>
2025-03-27 18:59:04 +0800

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -3,7 +3,11 @@ import java.util.Date;
 import java.util.List;
 
 public class Hotel extends Manager{
-
+    private String bookingId;
+    private Date startDate;
+    private Date endDate;
+    private double totalPrice;
+    
     public Hotel(int hotelId, String name, String location) {
         this.hotelId = hotelId;
         this.name = name;
